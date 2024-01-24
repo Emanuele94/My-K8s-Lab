1aed6bc7f869cca0507c4e9a86e5b03fb27a550e
Emanuele <34857243+Emanuele94@users.noreply.github.com>
2024-01-24 22:23:50 +0100

    Update init-master-k8s.sh

--- scripts/init-master-k8s.sh
+++ scripts/init-master-k8s.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # run as root
-# Specify the CIDR Range for Pods
-CIDR_RANGE="10.244.0.0/16"
 
 # Initialize the Kubernetes master node with kubeadm
-kubeadm init --pod-network-cidr=$CIDR_RANGE
+kubeadm init --config kubeadm-config.yaml
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
