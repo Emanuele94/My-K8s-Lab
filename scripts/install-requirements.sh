b7aca8c69191dfa7c7bb207aadfdb6c31780d864
Emanuele <34857243+Emanuele94@users.noreply.github.com>
2024-01-25 11:03:29 +0100

    Update install-requirements.sh

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -46,6 +46,7 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # set ip_forward to 1
+modprobe br_netfilter
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 echo "Kubernetes setup completed!"
