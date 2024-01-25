21454e84b6bceb33b8c93a9fe02e4c5d14c4210e
Emanuele <34857243+Emanuele94@users.noreply.github.com>
2024-01-25 17:25:37 +0100

    Update join-worker-k8s.sh

--- scripts/join-worker-k8s.sh
+++ scripts/join-worker-k8s.sh
@@ -2,3 +2,7 @@
 
 # from kubeadm init, get output im output-node end of file
 
+# example
+
+kubeadm join 172.26.0.3:6443 --token 6eaodn.fzk7sbwxqwezd2gb \
+        --discovery-token-ca-cert-hash sha256:2d7cec2915b26a2b4869783805cc78bf82ac0437fbbd033ed0c720d044f1b140
